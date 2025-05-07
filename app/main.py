27fe3b910fcd872e66225942a8be7035b4f39c95
HSK021843 <9902kim@hufs.ac.kr>
2025-05-07 13:24:37 +0900

    chore: 디렉터리 구조 수정

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +12,12 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+# uvicorn app.main:app
+if __name__ == "__main__":
+    uvicorn.run(
+        app="app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
