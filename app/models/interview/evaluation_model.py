3b8e6512263f550a93b0687847f6173bd6f83932
HAEUNP <superhaun@naver.com>
2025-06-12 15:52:53 +0900

    feat: 면접 dto 수정

--- app/models/interview/evaluation_model.py
+++ app/models/interview/evaluation_model.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 
 class EvaluationRequest(BaseModel):
     interviewId: int
-    interview_content: str
-    user_reply: str
+    interviewContent: str
+    userReply: str
